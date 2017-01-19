f5fd429587c1ae5eef2c16eecbbfdf97e30d5f6a
gilad shoham <shoham.gilad@gmail.com>
2017-01-19 18:05:29 +0200

    Check if account verified during login

--- src/verify.js
+++ src/verify.js
@@ -9,6 +9,9 @@ export default function verify(email, password, done) {
       if (!storedPassword)
         return done(null, false, "No password set");
 
+      if (!user.verified)
+        return done(null, false, "Account not verified");
+
       this.comparePassword(password, storedPassword, (err, match) => {
         if (err)
           done(err);
